1b398c84a7f87acce0a6e2277743bbf84725b173
tianjing <tianjing@xd.com>
2020-12-15 18:43:32 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
@@ -11,7 +9,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,40 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    logNames: {
+      astrolabe: '{"zh-CN":"星盘变化","zh-TW":"","en":""}',
+      card: '{"zh-CN":"插卡","zh-TW":"","en":""}',
+      change: '{"zh-CN":"公会改变","zh-TW":"","en":""}',
+      changeflag: '{"zh-CN":"玩家改名&安全密码","zh-TW":"","en":""}',
+      chat: '{"zh-CN":"聊天日志","zh-TW":"","en":""}',
+      complete: '{"zh-CN":"完成活动","zh-TW":"","en":""}',
+      consume: '{"zh-CN":"游戏币消耗","zh-TW":"","en":""}',
+      income: '{"zh-CN":"游戏币获取","zh-TW":"","en":""}',
+      item: '{"zh-CN":"物品获取","zh-TW":"","en":""}',
+      props: '{"zh-CN":"物品使用","zh-TW":"","en":""}'
+    },
+    logSearchCond: {
+      card: {
+        time: {
+          label: {
+            'zh-CN': '时间',
+            'zh-TW': '',
+            en: ''
+          },
+          type: 'timestamp',
+          content: ''
+        }
+      }
+    }
   },
   mutations: {
   },
   actions: {
+  },
+  getters: {
+    logNames: (state: any) => {
+      return state.logNames
+    }
   },
   modules: {
   }
